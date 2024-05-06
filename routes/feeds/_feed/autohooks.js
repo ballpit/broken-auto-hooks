d540d1b1f64fd5551f0d9b1d1a1bec1f96c52d9b
Bret Comnes <bcomnes@gmail.com>
2024-05-05 20:53:32 -0700

    Simplify autohook

--- routes/feeds/_feed/autohooks.js
+++ routes/feeds/_feed/autohooks.js
@@ -1,11 +1,3 @@
 export default async function (fastify, opts) {
-  // Add basic auth for feed and feed episode routes
-  fastify.register(import('@fastify/basic-auth'), {
-    validate,
-    authenticate: true
-  })
-
-  async function validate (uuid, token, request, reply) {
-    return true
-  }
+  fastify.decorate('wee', 'woo')
 }
